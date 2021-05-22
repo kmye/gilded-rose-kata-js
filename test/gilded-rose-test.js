cd99c4e2279ae46070fe3dbe60b938847c9fc8bd
Eileen Kang <eileen_kang@tech.gov.sg>
2021-05-22 17:32:09 +0800

    Add eslint

--- test/gilded-rose-test.js
+++ test/gilded-rose-test.js
@@ -1,11 +1,12 @@
-const {expect} = require('chai');
-const {Shop, Item} = require('../src/gilded-rose.js');
-describe("Gilded Rose", function() {
+const { expect } = require('chai');
+const { Shop, Item } = require('../src/gilded-rose');
 
-    it("should foo", function() {
-        const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
-        const items = gildedRose.updateQuality();
-        expect(items[0].name).to.equal("fixme");
-    });
-
+describe('Gilded Rose', () => {
+  it('should return the correct item name, sellin and quality', () => {
+    const gildedRose = new Shop([new Item('foo', 0, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).to.equal('foo');
+    expect(items[0].sellIn).to.equal(-1);
+    expect(items[0].sellIn).to.equal(-1);
+  });
 });
