f1c1ca4614eab241e34d9a9813b840906fc1734b
Eileen Kang <eileen_kang@tech.gov.sg>
2021-05-22 17:24:40 +0800

    original state of gilded rose kata

---
+++ test/gilded-rose-test.js
@@ -0,0 +1,11 @@
+const {expect} = require('chai');
+const {Shop, Item} = require('../src/gilded-rose.js');
+describe("Gilded Rose", function() {
+
+    it("should foo", function() {
+        const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].name).to.equal("fixme");
+    });
+
+});
