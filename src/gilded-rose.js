6e505232fc2729f9f960f8f2ff0820830447ec49
Eileen Kang <eileen_kang@tech.gov.sg>
2021-05-23 14:28:10 +0800

    Move update of sellin and quality to item class

--- src/gilded-rose.js
+++ src/gilded-rose.js
@@ -10,6 +10,18 @@ class Item {
     this.sellIn = sellIn;
     this.quality = quality;
   }
+
+  decreaseSellIn() {
+    this.sellIn -= 1;
+  }
+
+  decreaseQualityBy(amount) {
+    this.quality -= amount;
+  }
+
+  increaseQualityBy(amount) {
+    this.quality += amount;
+  }
 }
 
 class Shop {
@@ -22,7 +34,7 @@ class Shop {
       if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
         if (this.items[i].quality > MIN_QUANTITY) {
           if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1;
+            this.items[i].decreaseQualityBy(1);
           }
         }
       } else if (this.items[i].quality < MAX_QUANTITY) {
@@ -30,32 +42,32 @@ class Shop {
         if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
           if (this.items[i].sellIn <= SELLIN_THRESHOLD_QUALITY_INCREASE_BY_2) {
             if (this.items[i].quality < MAX_QUANTITY) {
-              this.items[i].quality = this.items[i].quality + 1;
+              this.items[i].increaseQualityBy(1);
             }
           }
           if (this.items[i].sellIn <= SELLIN_THRESHOLD_QUALITY_INCREASE_BY_3) {
             if (this.items[i].quality < MAX_QUANTITY) {
-              this.items[i].quality = this.items[i].quality + 1;
+              this.items[i].increaseQualityBy(1);
             }
           }
         }
       }
       if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
+        this.items[i].decreaseSellIn();
       }
       if (this.items[i].sellIn < 0) {
         if (this.items[i].name != 'Aged Brie') {
           if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
             if (this.items[i].quality > MIN_QUANTITY) {
               if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1;
+                this.items[i].decreaseQualityBy(1);
               }
             }
           } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
+            this.items[i].decreaseQualityBy(this.items[i].quality);
           }
         } else if (this.items[i].quality < MAX_QUANTITY) {
-          this.items[i].quality = this.items[i].quality + 1;
+          this.items[i].increaseQualityBy(1);
         }
       }
     }
